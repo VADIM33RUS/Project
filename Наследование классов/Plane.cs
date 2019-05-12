44ee200bcc4d9be55b249c56919659507c87c0d9
Вадим <ale244453942008@yandex.ru>
2019-05-12 21:34:01 +0300

    Реализовал наследование классов

---
+++ Наследование классов/Plane.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Наследование_классов
+{
+    class Plane
+    {
+        private double speed;
+        private double wingspan;
+    
+        public Plane()
+        {
+            speed = 0;
+            wingspan = 0;
+        }
+
+        public Plane(double _speed, double WingS)
+        {
+            speed = _speed;
+            wingspan = WingS;
+        }
+        public double Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                speed = value;
+            }
+        }
+        public double Wingspan
+        {
+            get
+            {
+                return wingspan;
+            }
+            set
+            {
+                wingspan = value;
+            }
+        }
+
+        public virtual void information()
+        {
+            Console.WriteLine($"Скорость: {Speed} км/ч ");
+            Console.WriteLine($"Размах крыла: {Wingspan} (метр.) ");
+        }
+    }
+
+    internal class Warplane : Plane
+    {
+        private int N_weapons;
+        private bool RLS;
+
+        public int N_weapons1 { get => N_weapons; set => N_weapons = value; }
+        public bool RLS1 { get => RLS; set => RLS = value; }
+
+        public Warplane(int n_weapons, bool radio,double _speed, double WingS) : base(_speed, WingS)
+        {
+            N_weapons = n_weapons;
+            RLS = radio;
+        }
+        public override void information()
+        {
+            base.information();
+            Console.WriteLine($"Кол-во подвесного вооружения: {N_weapons1} ,Радиолокационная станция: {RLS1}");
+        }
+    }
+
+    class Plane_fighter : Warplane
+    {
+        private string name;
+
+       
+        public Plane_fighter(string _name,int n_weapons, bool radio, double _speed, double WingS) : base(n_weapons,radio,_speed,WingS)
+        {
+            name = _name;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+
+            }
+        }
+        public override void information()
+        {
+            Console.WriteLine($"Название самолета: {Name}");
+            base.information();
+
+        }
+    }
+    class Plane_carrier : Warplane
+    {
+        private string name;
+        public Plane_carrier(string _name, int n_weapons, bool radio, double _speed, double WingS) : base(n_weapons, radio, _speed, WingS)
+        {
+            name = _name;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+
+            }
+        }
+        public override void information()
+        {
+            Console.WriteLine($"Название самолета: {Name}");
+            base.information();
+
+        }
+    }
+        
+
+
+}
---
+++ Наследование классов/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Наследование_классов
+{
+    
+
+    class Program
+    {
+        static void Main()
+        {
+            
+            System.Collections.Generic.List<Plane> planes = new System.Collections.Generic.List<Plane>();
+            planes.Add(new Plane_fighter("СУ-57", 9, true, 3000, 14));
+            planes.Add(new Plane_carrier("АН-26",0,true, 540,29.2));
+            for(int i = 0; i < planes.Count(); i++)
+            {
+                planes[i].information();
+            }
+
+            Plane_fighter F_plane = new Plane_fighter("ЛА-5", 3, false, 648, 9.8);
+            Plane_carrier C_plane = new Plane_carrier("ЛИ-2", 4, false, 290, 29.98);
+
+            F_plane.information();
+            C_plane.information();
+
+            Console.ReadKey();
+        }
+    }
+}
