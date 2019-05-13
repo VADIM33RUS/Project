044038771a4c38ef1ed778905acbfcc8e097dc26
Вадим <ale244453942008@yandex.ru>
2019-05-13 13:52:55 +0300

    Вывел на печать то, что вернул виртуальный метод

--- Наследование классов/Program.cs
+++ Наследование классов/Program.cs
@@ -12,21 +12,16 @@ namespace Наследование_классов
     {
         static void Main()
         {
-            
             System.Collections.Generic.List<Plane> planes = new System.Collections.Generic.List<Plane>();
             planes.Add(new Plane_fighter("СУ-57", 9, true, 3000, 14));
             planes.Add(new Plane_carrier("АН-26",0,true, 540,29.2));
-            for(int i = 0; i < planes.Count(); i++)
+            planes.Add(new Plane_fighter("ЛА-5", 3, false, 648, 9.8));
+            planes.Add(new Plane_carrier("ЛИ-2", 4, false, 290, 29.98));
+            for (int i = 0; i < planes.Count(); i++)
             {
                 planes[i].information();
+                Console.WriteLine("\n");
             }
-
-            Plane_fighter F_plane = new Plane_fighter("ЛА-5", 3, false, 648, 9.8);
-            Plane_carrier C_plane = new Plane_carrier("ЛИ-2", 4, false, 290, 29.98);
-
-            F_plane.information();
-            C_plane.information();
-
             Console.ReadKey();
         }
     }
