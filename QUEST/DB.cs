d3737d573f9a95e51b9c27ad539c7c6d9cd1eb58
Вадим <ale244453942008@yandex.ru>
2019-06-03 18:48:17 +0300

    Задание №3

---
+++ QUEST/DB.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace class_inheritance
+{
+    class DB
+    {
+
+        private SqlConnection connect;
+        public DB()
+        {
+            if (connect == null || connect.State == System.Data.ConnectionState.Closed)
+            {
+                Connection();
+            }
+            else
+            {
+                Console.WriteLine("Подключение уже установлено");
+            }
+
+        }
+        public void Connection()
+        {
+            connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\Desktop\Project\QUEST\Database1.mdf;Integrated Security=True");
+            connect.Open();
+        }
+        public void CloseConnection()
+        {
+            connect.Close();
+            connect.Dispose();
+        }
+        public void GetValue(List<Plane> Spisok)
+        {
+            SqlCommand command = new SqlCommand(@"SELECT ID,Name,Type,Weapons,Wingspan,Speed,RLS FROM [Table]", connect);
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    if (reader.HasRows)
+                        switch (reader.GetValue(2))
+                        {
+                            case "Plane_fighter":
+                                {
+                                    Spisok.Add(new Plane_fighter(reader));
+                                    break;
+                                }
+                            case "Plane_carrier":
+                                {
+                                    Spisok.Add(new Plane_carrier(reader));
+                                    break;
+                                }
+                        }
+                }
+            }
+            command.Dispose();
+        }
+        public void Output()
+        {
+            
+            SqlCommand command = new SqlCommand(@"SELECT ID,Name,Type,Weapons,Wingspan,Speed,RLS FROM [Table]", connect);
+            using (var reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n\n", reader.GetName(0), reader.GetName(1), reader.GetName(2), reader.GetName(3), reader.GetName(4), reader.GetName(5), reader.GetName(6));
+                    while (reader.Read())
+                    {
+                        var Id = reader.GetValue(0);
+                        var Name = reader.GetValue(1);
+                        var Type = reader.GetValue(2);
+                        var Wingspan = reader.GetValue(3);
+                        var Weapons = reader.GetValue(4);
+                        var Speed = reader.GetValue(5);
+                        var RLS = reader.GetValue(6);
+                        Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}", Id, Name, Type, Wingspan, Weapons, Speed, RLS);
+                    }
+                }
+            }
+            command.Dispose();
+        }
+    }
+}
--- QUEST/Plane.cs
+++ QUEST/Plane.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SqlClient;
 namespace class_inheritance
     
 {
     /// <summary>
     /// Класс описывающий самолёт
     /// </summary>
+    
     class Plane
     {
+
+        
         private double speed;
         private double wingspan;
+        
         public Plane()
         {
-            speed = 0;
-            wingspan = 0;
             
         }
         /// <summary>
@@ -29,7 +31,9 @@ namespace class_inheritance
         {
             speed = _speed;
             wingspan = WingS;
+            
         }
+        
         public double Speed
         {
             get
@@ -52,18 +56,21 @@ namespace class_inheritance
                 wingspan = value;
             }
         }
+        
         public virtual string information()
         {
             return $"Скорость: {Speed} км/ч  ,Размах крыла: {Wingspan} (метр.) ";
             
         }
     }
+    
     internal class Warplane : Plane
     {
         private int N_weapons;
         private bool RLS;
         public int N_weapons1 { get => N_weapons; set => N_weapons = value; }
         public bool RLS1 { get => RLS; set => RLS = value; }
+        public Warplane() : base() { }
         public Warplane(int n_weapons, bool radio,double _speed, double WingS) : base(_speed, WingS)
         {
             N_weapons = n_weapons;
@@ -74,13 +81,25 @@ namespace class_inheritance
             return base.information() + $"\n Кол-во подвесного вооружения: {N_weapons1} ,Радиолокационная станция: {RLS1}";
         }
     }
+    
     class Plane_fighter : Warplane
     {
+        private string type;
         private string name;
-        public Plane_fighter(string _name,int n_weapons, bool radio, double _speed, double WingS) : base(n_weapons,radio,_speed,WingS)
+        public void Serialize(SqlDataReader reader)
+        {
+            Name = reader.GetValue(1).ToString();
+            Speed = Convert.ToInt32(reader.GetValue(5));
+            Wingspan = Convert.ToInt32(reader.GetValue(3));
+        }
+        public Plane_fighter(SqlDataReader reader) : base()
+        {
+            Serialize(reader);
+        }
+        public Plane_fighter(string _name,int n_weapons, bool radio, double _speed, double WingS, string Type = "Fighter") : base(n_weapons,radio,_speed,WingS)
         {
             name = _name;
-
+            type = Type;
         }
         public string Name
         {
@@ -90,7 +109,7 @@ namespace class_inheritance
             }
             set
             {
-
+                name = value;
             }
         }
         public override string information()
@@ -98,12 +117,25 @@ namespace class_inheritance
             return $"Название самолета: {Name} " + base.information();
         }
     }
+    
     class Plane_carrier : Warplane
     {
+        private string type;
         private string name;
-        public Plane_carrier(string _name, int n_weapons, bool radio, double _speed, double WingS) : base(n_weapons, radio, _speed, WingS)
+        public void Serialize(SqlDataReader reader)
+        {
+            Name = reader.GetValue(1).ToString();
+            Speed = Convert.ToInt32(reader.GetValue(5));
+            Wingspan = Convert.ToInt32(reader.GetValue(3));
+        }
+        public Plane_carrier(SqlDataReader reader) : base()
+        {
+            Serialize(reader);
+        }
+        public Plane_carrier(string _name, int n_weapons, bool radio, double _speed, double WingS, string Type = "Carrier") : base(n_weapons, radio, _speed, WingS)
         {
             name = _name;
+            type = Type;
         }
         public string Name
         {
@@ -113,7 +145,7 @@ namespace class_inheritance
             }
             set
             {
-
+                name = value;
             }
         }
         public override string information()
